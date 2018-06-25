b67d8f2daf7e53b2d1051b22e983ebe6af05af87
Pratik44 <pratik.deenbandhu456@gmail.com>
2018-06-25 16:28:03 +0530

    adding jenkins file

--- installkubernets.sh
+++ installkubernets.sh
@@ -100,3 +100,125 @@ subjects:
   namespace: kube-system
 EOF
 kubectl create -f kube-access-dashboard.yaml
+
+cat <<EOF > default-backend-deployment.yaml
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: default-http-backend
+  labels:
+    k8s-app: default-http-backend
+  namespace: kube-system
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        k8s-app: default-http-backend
+    spec:
+      terminationGracePeriodSeconds: 60
+      containers:
+      - name: default-http-backend
+        # Any image is permissable as long as:
+        # 1. It serves a 404 page at /
+        # 2. It serves 200 on a /healthz endpoint
+        image: gcr.io/google_containers/defaultbackend:1.0
+        livenessProbe:
+          httpGet:
+            path: /healthz
+            port: 8080
+            scheme: HTTP
+          initialDelaySeconds: 30
+          timeoutSeconds: 5
+        ports:
+        - containerPort: 8080
+        resources:
+          limits:
+            cpu: 10m
+            memory: 20Mi
+          requests:
+            cpu: 10m
+            memory: 20Mi
+EOF
+
+cat <<EOF > default-backend-service.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  name: default-http-backend
+  namespace: kube-system
+  labels:
+    k8s-app: default-http-backend
+spec:
+  ports:
+  - port: 80
+    targetPort: 8080
+  selector:
+    k8s-app: default-http-backend
+  type: ClusterIP
+EOF
+
+cat <<EOF > nginx-ingress-controller-service.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  name: nginx-ingress-controller
+  namespace: kube-system
+spec:      
+  # Because of the type of NodePort, arbitrary port in specific range will be opened.
+  type: NodePort
+  ports:
+    - port: 80
+      name: http
+    - port: 443
+      name: https
+  selector:
+    k8s-app: nginx-ingress-controller
+EOF
+
+cat <<EOF > tomcat8-jenkins-deployment.yaml
+apiVersion: apps/v1beta1
+kind: Deployment
+metadata:
+  labels:
+    k8s-app: tomcat8-jenkins
+  name: tomcat8-jenkins
+  namespace: default
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        k8s-app: tomcat8-jenkins
+      name: tomcat8-jenkins
+    spec:
+      containers:
+        -
+          args:
+          - daemon
+          # Fetch from Docker Hub
+          image: "skcho4docker/tomcat8-jenkins-path:ver1.6.3"
+          name: tomcat8-jenkins-path
+          ports:
+            -
+              containerPort: 8080
+              protocol: TCP
+EOF
+
+cat <<EOF > tomcat8-jenkins-service.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  name: tomcat8-jenkins
+  namespace: default
+  labels:
+    k8s-app: tomcat8-jenkins
+spec:
+  ports:
+  - port: 80
+    targetPort: 8080
+    protocol: TCP
+  selector:
+    k8s-app: tomcat8-jenkins
+  type: ClusterIP
+EOF
