d129086e5765bef9b492c69e03fbff956b79d93c
Pratik44 <pratik.deenbandhu456@gmail.com>
2018-06-26 16:29:27 +0530

    added helm

--- installkubernets.sh
+++ installkubernets.sh
@@ -222,3 +222,6 @@ spec:
     k8s-app: tomcat8-jenkins
   type: ClusterIP
 EOF
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
